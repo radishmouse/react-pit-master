2a49f7b4cbe5d9e71dfa2d1a2564e58b6e59d34f
Chris Aquino <caquino@bignerdranch.com>
2017-03-18 18:45:23 -0400

    adds a grill and sensor simulator

---
+++ src/lib/GrillSimulator.js
@@ -0,0 +1,66 @@
+
+/*
+
+given an initial temp for food
+and a current temp for oven
+it should incrementally increase food temp
+*/
+
+const COOK_FACTOR = 0.063; // fake percentage increase of difference
+
+const incrementFoodTemp = (foodTemp, ovenTemp) => {
+  return (foodTemp + ((ovenTemp - foodTemp) * COOK_FACTOR));
+}
+
+function * cookFood (foodTemp, ovenTemp) {
+  console.log(`cooking: food: ${foodTemp.toFixed(2)}, oven: ${ovenTemp}`);
+  while (foodTemp < ovenTemp) {
+    foodTemp = incrementFoodTemp(foodTemp, ovenTemp);
+    yield foodTemp;
+  }
+}
+
+class Sensor {
+  constructor(sampleGenerator, interval=1000) {
+    this.values = [];
+    this.sampleGenerator = sampleGenerator;
+    this.sampleInterval = null;
+    this.interval = interval;
+  }
+
+  sample() {
+    let val = this.sampleGenerator.next().value;
+    // console.log(`sampled ${val}`);
+    this.values.push(val);
+  }
+
+  start() {
+    if (this.sampleInterval) {
+      return;
+    }
+
+    this.sampleInterval = setInterval(this.sample.bind(this), this.interval);
+  }
+
+  stop() {
+    clearInterval(this.sampleInterval);
+  }
+
+  minutesAgo(howMany) {
+    // assuming a 1 second sampling rate
+    // 1 minute ago is [length - 60]
+    // 5 minutes ago is [length - 300]
+    // 10 minutes ago is [length - 600]
+
+    // but really, we'll fudge it
+    // and divide by 10
+    let index = this.values.length - (howMany * 6);
+    return index >= 0 ? this.values[index] : '--'
+  }
+
+}
+
+module.exports = {
+  cookFood,
+  Sensor
+};
