949ab0066b24a68902ffd100df85afd032ade8fd
Chris Aquino <caquino@bignerdranch.com>
2017-03-18 10:44:59 -0400

    fixes missing key

--- src/containers/TemperatureHistory.js
+++ src/containers/TemperatureHistory.js
@@ -3,8 +3,9 @@ import Readout from './Readout';
 
 const TemperatureHistory = ({valueArray=[1, 5, 10]}) => (
   // we should make this a flex container
+  // note that using the index is considered a last resort.
   <div>
-    {valueArray.map((val) => <Readout value={val} />)}
+    {valueArray.map((val, i) => <Readout key={i} value={val} />)} 
   </div>
 );
 
