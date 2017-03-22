42ae05c24608366a07150b0f9e4cbb503a9e03cd
Chris Aquino <caquino@bignerdranch.com>
2017-03-22 04:33:58 -0400

    adds moving arrow

--- src/containers/Monitor.js
+++ src/containers/Monitor.js
@@ -5,7 +5,13 @@ import Readout from './Readout';
 
 import closeButton from './close.svg';
 import gradient from './gradient.svg';
+import whiteArrow from './white-arrow.svg';
+import blueArrow from './blue-arrow.svg';
 import './Monitor.css';
+
+const targetArrow = {
+  bottom: '178'
+};
 
 const Monitor = ({
   id,
@@ -16,27 +22,34 @@ const Monitor = ({
   ovenTemperature,
   changeHandler,
   closeHandler
-}) => (
-  <div className="monitor">
-    <div className="monitorHeader">
-      <NameLabel 
-        name={`${food} for: ${name}`} 
-      />
-      <div className="closeMonitorButton" onClick={(e) => closeHandler(id)}>
-        <img src={closeButton} alt='X' />
+}) => {
+  const currentArrow = {
+    bottom: (foodTemperature / ovenTemperature) * 178
+  };
+  return (
+    <div className="monitor">
+      <div className="monitorHeader">
+        <NameLabel 
+          name={`${food} for: ${name}`} 
+        />
+        <div className="closeMonitorButton" onClick={(e) => closeHandler(id)}>
+          <img src={closeButton} alt='X' />
+        </div>
+      </div>
+      <div className="monitorBody">
+        <div className="monitorTemperatures">
+          <Readout className="" deemphasize={true} value={ovenTemperature} label="Target" />
+          <Readout className="" emphasize={true} value={foodTemperature} label="Current" />
+          <TemperatureHistory valueArray={historyArray} />
+        </div>
+        <div className="monitorGraph">
+          <img src={gradient} alt="" />
+          <img style={targetArrow} className="arrow" src={whiteArrow} alt="white arrow" />
+          <img style={currentArrow} className="arrow" src={blueArrow} alt="blue arrow" />
+        </div>
       </div>
     </div>
-    <div className="monitorBody">
-      <div className="monitorTemperatures">
-        <Readout className="" deemphasize={true} value={ovenTemperature} label="Target" />
-        <Readout className="" emphasize={true} value={foodTemperature} label="Current" />
-        <TemperatureHistory valueArray={historyArray} />
-      </div>
-      <div className="monitorGraph">
-        <img src={gradient} alt="" />
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Monitor;
